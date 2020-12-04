44d40807fd39e73e0b6a8faa8529e120150c0e21
Juan G. Molina <jg.molina92@hotmail.com>
2020-12-03 19:54:18 -0500

    Add HeroScreen tests

--- src/tests/components/heroes/HeroScreen.test.js
+++ src/tests/components/heroes/HeroScreen.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { HeroScreen } from '../../../components/heroes/HeroScreen';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 
 describe('Pruebas en <HeroScreen />', () => {
   const historyMock = {
@@ -10,13 +10,75 @@ describe('Pruebas en <HeroScreen />', () => {
     goBack: jest.fn(),
   };
 
-  const wrapper = mount(
-    <MemoryRouter initialEntries={['/hero']}>
-      <HeroScreen history={historyMock} />
-    </MemoryRouter>
-  );
-
   test('Debe de mostrar el componente Redirect si no hay argumentos en el URL', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero']}>
+        <HeroScreen history={historyMock} />
+      </MemoryRouter>
+    );
+
     expect(wrapper.find('Redirect').exists()).toBe(true);
+  });
+
+  test('Debe de mostrar un hero si el parámetro existe y se encuentra el hero', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/dc-batman']}>
+        <Route path='/hero/:heroId' component={HeroScreen} />
+      </MemoryRouter>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.row').exists()).toBe(true);
+  });
+
+  test('Debe de regresar a la pantalla principal del hero con push', () => {
+    const historyMock = {
+      length: 1,
+      push: jest.fn(),
+      goBack: jest.fn(),
+    };
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/dc-batman']}>
+        <Route
+          path='/hero/:heroId'
+          component={() => <HeroScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    );
+
+    wrapper.find('button').prop('onClick')();
+
+    expect(historyMock.push).toHaveBeenCalledWith('/dc');
+    expect(historyMock.goBack).not.toHaveBeenCalled();
+  });
+
+  test('Debe de regresar a la pantalla anterior con goBack', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/dc-batman']}>
+        <Route
+          path='/hero/:heroId'
+          component={() => <HeroScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    );
+
+    wrapper.find('button').prop('onClick')();
+
+    expect(historyMock.goBack).toHaveBeenCalled();
+    expect(historyMock.push).toHaveBeenCalledTimes(0);
+  });
+
+  test('Debe de llamar el redirect si el hero no existe', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/dc-batsy']}>
+        <Route
+          path='/hero/:heroId'
+          component={() => <HeroScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.text()).toBe('');
   });
 });
