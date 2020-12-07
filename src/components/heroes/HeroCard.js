766376a8d2ada71c3cb5ecb111bd85abf3ab9409
Juan G. Molina <jg.molina92@hotmail.com>
2020-12-07 12:14:39 -0500

    Add flip effect to cards

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './heroCard.css';
 
 export const HeroCard = ({
   id,
@@ -10,24 +11,30 @@ export const HeroCard = ({
   characters,
 }) => {
   return (
-    <div className='card ms-3' style={{ maxWidth: 540 }}>
-      <div className='row no-gutters'>
-        <div className='col-md-4'>
+    <div className='card-container col-md-4 col-sm-6'>
+      <div className='card-flip'>
+        <div className='card front'>
           <img
             src={`./assets/heroes/${id}.jpg`}
             className='card-img'
             alt={superhero}
           />
         </div>
-        <div className='col-md-8'>
-          <div className='card-body'>
+        <div className='card back p-4'>
+          <div className='card-block'>
             <h5 className='card-title'>{superhero}</h5>
-            <p className='card-text'>{alter_ego}</p>
+            <p className='card-text'>
+              <b>Alter Ego:</b> {alter_ego}
+            </p>
             {alter_ego !== characters && (
-              <p className='card-text'>{characters}</p>
+              <p className='card-text'>
+                <b>Characters:</b> {characters}
+              </p>
             )}
             <p className='card-text'>
-              <small className='text-muted'>{first_appearance}</small>
+              <small className='text-muted'>
+                <b>First Appeatance:</b> {first_appearance}
+              </small>
             </p>
             <Link to={`./hero/${id}`}>More...</Link>
           </div>
